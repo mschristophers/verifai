50a7cfb656257ab6f681a45e2421ba780da951db
Christopher Simanjuntak <manuelstefan150@gmail.com>
2023-06-25 01:34:07 -0400

    added worldcoin stuff

--- src/views/Verification.jsx
+++ src/views/Verification.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { CredentialType, IDKitWidget, ISuccessResult } from "@worldcoin/idkit";
 import './Verification.css';
 
 const Verification = () => {
+  const handleProof = (result) => {
+		return new Promise((resolve) => {
+			setTimeout(() => resolve(), 3000);
+		});
+	};
+
+	const onSuccess = (result) => {
+		console.log(result);
+	};
+
+	const urlParams = new URLSearchParams(window.location.search);
+	const credential_types = (urlParams.get("credential_types")?.split(",")) ?? [
+		CredentialType.Orb,
+		CredentialType.Phone,
+	];
+
+	const action = urlParams.get("action") ?? "";
+	const app_id = urlParams.get("app_id") ?? "wid_staging_1234";
+
+
   return (
     <div className="verification-container">
       <h1>Identity Verification</h1>
